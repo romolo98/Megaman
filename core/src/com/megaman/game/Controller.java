47cea1e7400094caba703793136fc9fb7e3865c0
romolo98 <romolo.aiello1998@gmail.com>
2019-06-11 20:31:00 +0200

    Added jump
    bug fixed and code improvement.

--- core/src/com/megaman/game/Controller.java
+++ core/src/com/megaman/game/Controller.java
@@ -1,18 +1,76 @@
 package com.megaman.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 
 public class Controller {
 	
+	public static final int WALK = 0;
+	public static final int JUMP = 1;
+	public static final int FALL = 2;
+	public static final int SHOOT = 3;
+	public static final int WALK_SHOOT = 4;
+	public static final int WALK_JUMP = 5;
+	public static final int WALK_START = 6;
+	
+	boolean[] controlli;
+	int speed;
+	int contJump;
+	
 	public Controller () {
+		controlli = new boolean[7];
+		speed = 2;
+		contJump = 0;
 	}
 
-	public void muoviMegaman (int keycode,Megaman megaman) {
-		if (keycode == Input.Keys.SPACE) {
-			megaman.setPositionY(megaman.getPositionY()+2);
+	public void muoviMegaman (Megaman megaman) {
+		if (Gdx.input.isKeyJustPressed(Keys.RIGHT)) {
+			megaman.setPositionX(megaman.getPositionX()+1);
+			controlli[WALK_START] = true;
 		}
-		else {
-			megaman.setPositionY(megaman.getPositionY()-2);
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			if (Gdx.input.isKeyPressed(Keys.SPACE)) {
+				megaman.setPositionX(megaman.getPositionX()+speed);
+				megaman.setPositionY(megaman.getPositionY()+speed);
+				controlli[WALK_JUMP] = true;
+			}
+			else {
+				megaman.setPositionX(megaman.getPositionX()+speed);
+				controlli[WALK] = true;
+			}
+		}
+		if (Gdx.input.isKeyJustPressed(Keys.SPACE)) {
+			controlli[JUMP] = true;
+		}
+		
+		if (controlli[JUMP]) {
+			megaman.setPositionY(megaman.getPositionY()+speed);
+		}
+			
+		if (controlli[FALL]) {
+			controlli[JUMP] = false;
+			megaman.setPositionY(megaman.getPositionY()-speed);
+			if (megaman.getPositionY() < 0) {
+				controlli[FALL] = false;
+			}
 		}
 	}
+	
+	void setTuttiControlliFalse () {
+		for (int i=0;i<6;i++)
+		controlli[i] = false;
+	}
+	void setControlliFalse (int index) {
+		controlli[index] = false;
+	}
+	
+	boolean getControlli (int index) {
+		return controlli[index];
+	}
+	
+	void setFallTrue () {
+		controlli[FALL] = true;
+	}
+	
 }
--- core/src/com/megaman/game/GraphicsManager.java
+++ core/src/com/megaman/game/GraphicsManager.java
@@ -11,9 +11,11 @@ public class GraphicsManager {
 	Megaman megaman;
 	Controller controller;
 	float elapsed;
+	float salto;
 	
 	public GraphicsManager (){
 		elapsed = 0;
+		salto = 0;
 		controller = new Controller();
 		megaman = new Megaman();
 		graphicLoader = new GraphicLoader();
@@ -21,20 +23,46 @@ public class GraphicsManager {
 	}
 	public void draw(SpriteBatch batch) {
 		elapsed += Gdx.graphics.getDeltaTime();
+		controller.muoviMegaman(megaman);
 		
-		if (Gdx.input.isKeyJustPressed(Keys.RIGHT)){
+		if (controller.controlli[controller.WALK_START]) {
 			batch.draw(graphicLoader.getInizioWalk(),megaman.getPositionX(),megaman.getPositionY());
+			controller.setControlliFalse(controller.WALK_START);
 		}
-		else if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			batch.draw(graphicLoader.getWalk().getKeyFrame(elapsed, true), megaman.getPositionX(), megaman.getPositionY());
+		if (controller.getControlli(controller.FALL)) {
+			batch.draw(graphicLoader.getFall().getKeyFrame(elapsed, true), megaman.getPositionX(), megaman.getPositionY());
 		}
-		else if (Gdx.input.isKeyPressed(Keys.SPACE)) {
-			controller.muoviMegaman(Keys.SPACE, megaman);
-			batch.draw(graphicLoader.getJump().getKeyFrame(elapsed, true), megaman.getPositionX(), megaman.getPositionY());
+		if (controller.getControlli(controller.WALK)) {
+			if (controller.getControlli(controller.WALK_JUMP)) {
+				controller.setControlliFalse(controller.WALK);
+				salto+= Gdx.graphics.getDeltaTime();
+				batch.draw(graphicLoader.getJump().getKeyFrame(elapsed, true), megaman.getPositionX(), megaman.getPositionY());
+				if (graphicLoader.getJump().isAnimationFinished(salto)) {
+					controller.setControlliFalse(controller.WALK_JUMP);
+					salto = 0;
+					controller.setFallTrue();
+				}
+			}
+			else if (!controller.getControlli(controller.FALL)){ 
+					batch.draw(graphicLoader.getWalk().getKeyFrame(elapsed, true), megaman.getPositionX(),megaman.getPositionY());
+					controller.setControlliFalse(controller.WALK_JUMP);					
+					controller.setControlliFalse(controller.WALK);
+			}
+		}
+		if (controller.getControlli(controller.JUMP)) {
+			batch.draw(graphicLoader.getJump().getKeyFrame(elapsed), megaman.getPositionX(), megaman.getPositionY());
+			salto+= Gdx.graphics.getDeltaTime();
+			if (graphicLoader.getJump().isAnimationFinished(salto)) {
+				controller.setControlliFalse(controller.JUMP);
+				salto = 0;
+				controller.setFallTrue();
+			}
 		}
 		else {
+			controller.setTuttiControlliFalse();
 			batch.draw(graphicLoader.getIdle().getKeyFrame(elapsed/2,true), megaman.getPositionX(), megaman.getPositionY());
 		}
+	
 		
 	}
 }
